d9a2f99f78aa136a1024ed6aa3b3c3e663dc02d7
sattbuttar <45004111+sattbuttar@users.noreply.github.com>
2018-11-13 18:27:53 +0530

    Add files via upload

---
+++ SatwinderSingh ITD18035/A2SatwinderSinghP1/A2SatwinderSinghP1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2SatwinderSinghP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*STEP 1*/
+
+
+            Console.WriteLine("Press 1 for CANADIAN CITIZEN and 2 for INTERNATIONAL STUDENT");
+            string g = Console.ReadLine();
+            double m = double.Parse(g);
+            
+
+            if (m == 1)
+            {
+                Console.WriteLine("Please enter age");
+                string h = Console.ReadLine();
+                double n = double.Parse(h);
+                if (n <= 18)
+                {
+                    Console.WriteLine("Base fee ($300) + 13% HST");
+                    double a = 300 + (0.13 * 300);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+                else if (n <= 49)
+                {
+                    Console.WriteLine("Base fee ($500) + 13% HST");
+                    double a = 500 + (0.13 * 500);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+                else
+                {
+                    Console.WriteLine("Base fee ($400) + 13% HST");
+                    double a = 400 + (0.13 * 400);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+            }
+
+            else if (m == 2)
+            {
+                Console.WriteLine("Please enter age");
+                string j = Console.ReadLine();
+                double k = double.Parse(j);
+                if (k <= 18)
+                {
+                    Console.WriteLine("Base fee ($300+$100=$400) + 13% HST");
+                    double a = 400 + (0.13 * 400);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+                else if (k <= 49)
+                {
+                    Console.WriteLine("Base fee ($500+$100=$600) + 13% HST");
+                    double a = 600 + (0.13 * 600);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Base fee ($400+$100=$500) + 13% HST");
+                    double a = 500 + (0.13 * 500);
+                    Console.WriteLine("Student should pay $ " + a);
+                }
+
+
+            }
+
+            /*STEP 2*/
+
+            Console.WriteLine("Student should also pay registration fee");
+            Console.WriteLine("Enter 7 for FALL month (Sept., Oct., Nov., or Dec.) or" +
+                "Enter 8 for WINTER month (Jan., Feb., Mar. or Apr.) or" +
+                "Enter 9 for SUMMER month (May, Jun., Jul. or Aug.)");
+            string s = Console.ReadLine();
+            double x;
+            switch(s)
+            {
+                case "7":
+                    {
+                        Console.WriteLine("$250 + 13% HST");
+                        x =250+(0.13*250);
+                        Console.WriteLine("Registration fee is : $" + x);
+                        break;
+                    }
+
+                case "8":
+                    {
+                        Console.WriteLine("$220 + 13% HST");
+                        x = 220 + (0.13 * 220); 
+                        Console.WriteLine("Registration fee is : $" + x);
+                        break;
+                    }
+
+                case "9":
+                    {
+                        Console.WriteLine("$150 + 13% HST");
+                        x = 150 + (0.13 * 150); 
+                        Console.WriteLine("Registration fee is : $" + x);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("please enter only given value");
+                        break;
+                    }
+            }
+
+            
+
+
+        }
+
+
+
+
+    }
+}
+    
---
+++ SatwinderSingh ITD18035/A2SatwinderSinghP2/A2SatwinderSinghP2/A2SatwinderSinghP2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2SatwinderSinghP2
+{
+    class Program
+    {
+        static double rec(double r , double p)
+        {
+            double rec;
+            rec = 4 / 3 * p * r * r * r;
+            return rec;
+        }
+        static double rec(double g,double h, double p)
+        {
+            double rec;
+            rec = p * g * g *h;
+            return rec;
+        }
+        static int rec(int l, int b, int h)
+        {
+            int rec;
+            rec = l * b * h ;
+            return rec;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 for sphere or");
+            Console.WriteLine("2 for cylinder or");
+            Console.WriteLine("3 for rectangular prism");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+
+
+            if(y==1)
+            {
+                Console.WriteLine("enter radius");
+                string u = Console.ReadLine();
+                double r = double.Parse(u);
+                double result;
+                result = rec(3.14, r);
+                Console.WriteLine(" volume of sphere is = " + result);
+            }
+
+            else if (y==2)
+            {
+                Console.WriteLine("enter radius");
+                string i = Console.ReadLine();
+                double g = double.Parse(i);
+                Console.WriteLine("enter height");
+                string k = Console.ReadLine();
+                double h = double.Parse(k);
+                double res;
+                res = rec(3.14, g, h);
+                Console.WriteLine(" volume of cylinder is = " + res);
+            }
+
+            else if (y==3)
+            {
+                Console.WriteLine("enter length");
+                string a = Console.ReadLine();
+                int l = int.Parse(a);
+                Console.WriteLine("enter breadth");
+                string c = Console.ReadLine();
+                int b = int.Parse(c);
+                Console.WriteLine("enter height");
+                string d = Console.ReadLine();
+                int h = int.Parse(d);
+                int resu;
+                resu = rec(l, b, h);
+                Console.WriteLine(" volume of rectangular prism is = " + resu);
+            }
+        }
+    }
+}
